cd7be65e1313e8a75544cd4c902351073168fadd
plizak <>
2021-02-02 22:10:59 +0100

    Initial commit

---
+++ MainSDK/Core.swift
@@ -0,0 +1,18 @@
+//
+//  Core.swift
+//  MainSDK
+//
+//  Created by Peter Lizak on 02/02/2021.
+//
+
+import Foundation
+
+public class Core {
+    public init() {
+        print("Core MainSDK init called")
+    }
+
+    public func test() {
+        print("Core MainSDK test called")
+    }
+}
---
+++ MainSDK/MainSDK.h
@@ -0,0 +1,18 @@
+//
+//  MainSDK.h
+//  MainSDK
+//
+//  Created by Peter Lizak on 02/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MainSDK.
+FOUNDATION_EXPORT double MainSDKVersionNumber;
+
+//! Project version string for MainSDK.
+FOUNDATION_EXPORT const unsigned char MainSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MainSDK/PublicHeader.h>
+
+
